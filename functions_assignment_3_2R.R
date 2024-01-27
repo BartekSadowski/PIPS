4d02556d79145770bfd3b2bb9d5812896a21bbda
BartekSadowski <157648182+BartekSadowski@users.noreply.github.com>
2024-01-27 02:43:54 +0100

    Add files via upload

--- functions_assignment_3_2R.R
+++ functions_assignment_3_2R.R
@@ -52,8 +52,8 @@ make_art <- function(seed,
   }
       
   #Setting canvas
-  art <<- matrix(nrow = outer_canvas, ncol = outer_canvas)
-  initial <<- floor((outer_canvas - inner_canvas) / 2) + 1
+  art <- matrix(nrow = outer_canvas, ncol = outer_canvas)
+  initial <- floor((outer_canvas - inner_canvas) / 2) + 1
   
   if (art_type != "lines" && art_type != "angles" && art_type != "squares" && art_type != "patches") {
     art_type <- sample(c("lines", "angles", "squares", "patches"),1)
@@ -62,25 +62,25 @@ make_art <- function(seed,
   print(paste0("art type: ", art_type))
   print("creating...")
   if (art_type == "lines") {
-    art[initial:(outer_canvas - initial), initial] <<- 500
+    art[initial:(outer_canvas - initial), initial] <- 500
     for(i in initial:(inner_canvas + initial - 1)) {
       for(j in ((initial + 1):(inner_canvas + initial - 1))) {
-        art[i,j] <<- art[i, j - 1] + rnorm(1, mean = 0, sd = 50)
+        art[i,j] <- art[i, j - 1] + rnorm(1, mean = 0, sd = 50)
       }
     }
   }
 
   if (art_type == "angles") {
-    art[initial, initial] <<- 500
+    art[initial, initial] <- 500
     for (i in initial:(inner_canvas + initial - 2)) {
       for (j in initial:i) {
         if (i == j) {
-          art[i, j + 1] <<- art[i, i] + rnorm(1, mean = 0, sd = 50)
-          art[i + 1, j + 1] <<- art[i, i] + rnorm(1, mean = 0, sd = 50)
-          art[i + 1, j] <<- art[i, i] + rnorm(1, mean = 0, sd = 50)
+          art[i, j + 1] <- art[i, i] + rnorm(1, mean = 0, sd = 50)
+          art[i + 1, j + 1] <- art[i, i] + rnorm(1, mean = 0, sd = 50)
+          art[i + 1, j] <- art[i, i] + rnorm(1, mean = 0, sd = 50)
         }  else {
-          art[j, i + 1] <<- art[j, i] + rnorm(1, mean = 0, sd = 50)
-          art[i + 1, j] <<- art[i, j] + rnorm(1, mean = 0, sd = 50)
+          art[j, i + 1] <- art[j, i] + rnorm(1, mean = 0, sd = 50)
+          art[i + 1, j] <- art[i, j] + rnorm(1, mean = 0, sd = 50)
           
         }
       }
@@ -89,18 +89,18 @@ make_art <- function(seed,
   
   if (art_type == "squares") {
     number_of_colors <- sample(5:20, 1)
-    square_colors <<- 1:number_of_colors
-    square_size <<- sample(floor(inner_canvas/500):floor(inner_canvas/10), 1)
-    squares_in_line <<- floor(inner_canvas / square_size)
-    coords <<- c((outer_canvas - inner_canvas) / 2, (outer_canvas - inner_canvas) / 2)
+    square_colors <- 1:number_of_colors
+    square_size <- sample(floor(inner_canvas/500):floor(inner_canvas/10), 1)
+    squares_in_line <- floor(inner_canvas / square_size)
+    coords <- c((outer_canvas - inner_canvas) / 2, (outer_canvas - inner_canvas) / 2)
     for(i in 1:squares_in_line) {
       for(j in 1:squares_in_line) {
-        art[coords[1]:(coords[1]+square_size), coords[2]:(coords[2]+square_size)] <<- sample(square_colors, 1)
-        coords <<- coords + c(square_size, 0)
+        art[coords[1]:(coords[1]+square_size), coords[2]:(coords[2]+square_size)] <- sample(square_colors, 1)
+        coords <- coords + c(square_size, 0)
       }
-      coords <<- c((outer_canvas - inner_canvas) / 2, coords[2] + square_size)
+      coords <- c((outer_canvas - inner_canvas) / 2, coords[2] + square_size)
     }
-    fillings <<- sample(colors(), number_of_colors)
+    fillings <- sample(colors(), number_of_colors)
     rotate <- sample(c(TRUE, FALSE),1)
     if(rotate) {
       angle <- runif(1, min = 0, max = 360)
@@ -112,19 +112,19 @@ make_art <- function(seed,
   
   if (art_type == "patches") {
     number_of_colors <- sample(5:20, 1)
-    fillings <<- sample(colors(), number_of_colors)
-    iterations <<- sample(5:150, 1)
+    fillings <- sample(colors(), number_of_colors)
+    iterations <- sample(5:150, 1)
     for (i in 1:iterations)
     {
-      blob_color <<- sample(1:number_of_colors,1)
-      size <<- sample(10:300, 1)
-      blob <<- runif(2, initial, inner_canvas - size + initial)
+      blob_color <- sample(1:number_of_colors,1)
+      size <- sample(10:300, 1)
+      blob <- runif(2, initial, inner_canvas - size + initial)
       for (j in 1:size) {
         for(k in 1:size) {
-          p <<- (size - min(c(j,k)) - 1)/size
-          draw <<- sample(1:100, 1)
+          p <- (size - min(c(j,k)) - 1)/size
+          draw <- sample(1:100, 1)
           if(100*p > draw) {
-            art[blob[1]+j, blob[2]+k] <<- blob_color
+            art[blob[1]+j, blob[2]+k] <- blob_color
           }
         }
       }
@@ -133,10 +133,10 @@ make_art <- function(seed,
   
   #Plotting
   if (art_type == "squares" || art_type == "patches") {
-    art_data <<- reshape2::melt(art)
+    art_data <- reshape2::melt(art)
     print("plotting...")
     x11()
-    p <<- ggplot(art_data, aes(x = Var2, y = Var1)) + 
+    p <- ggplot(art_data, aes(x = Var2, y = Var1)) + 
       geom_raster(aes(fill=factor(round(value)))) + 
       scale_fill_manual(values=fillings, na.value = "white") + 
       theme(axis.line=element_blank(), axis.text.x=element_blank(),
@@ -150,7 +150,7 @@ make_art <- function(seed,
   }
   gradient_colors <- sample(colors(),2)
   if (art_type == "lines" || art_type == "angles") {
-    art_data <<- melt(art)
+    art_data <- melt(art)
     print("plotting...")
     x11()
     p <- ggplot(art_data, aes(x = Var2, y = Var1)) + 
